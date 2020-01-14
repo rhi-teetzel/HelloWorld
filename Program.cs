edc566cd6c1469ba6bb615233be9d01cdf039c89
rhi-teetzel <rhiannateetzel@gmail.com>
2020-01-14 14:22:03 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+            
+        {
+            string yourName;
+            Console.WriteLine("Hello World! What is your name?");
+
+            yourName = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}", yourName);
+           
+           
+        }
+    }
+}
